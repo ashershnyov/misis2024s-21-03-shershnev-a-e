f73733263ead149642c87f34d47aaf2db861a575
ashershnyov <antshersh@gmail.com>
2024-06-09 13:47:57 +0300

    lab08 md + new pics

--- prj.lab/lab08/lab08.cpp
+++ prj.lab/lab08/lab08.cpp
@@ -3,49 +3,18 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 
-// cv::Mat kRotZ = (cv::Mat_<cv::Vec3f>(3, 1) << 
-//     cv::Vec3f{float(std::cos(45)), 0, float(std::sin(45))},
-//     cv::Vec3f{0, 1, 0},
-//     cv::Vec3f{float(-std::sin(45)), 0, float(std::cos(45))}
-// );
 
-cv::Mat kRotY = (cv::Mat_<float>(3, 3) << 
-    float(std::cos(45)), 0, float(std::sin(45)),
-    0, 1, 0,
-    float(-std::sin(45)), 0, float(std::cos(45))
+cv::Mat kProj = (cv::Mat_<float>(2, 3) << 
+    0.5 + 0.5 / cv::sqrt(3), -0.5 + 0.5 / cv::sqrt(3), -1 / cv::sqrt(3),
+    -0.5 + 0.5 / cv::sqrt(3), 0.5 + 0.5 / cv::sqrt(3), -1 / cv::sqrt(3)
 );
 
-cv::Mat kRotX = (cv::Mat_<float>(3, 3) << 
-    1, 0, 0,
-    0, float(std::cos(45)), float(-std::sin(45)),
-    0, float(std::sin(45)), float(std::cos(45))
-);
 
-// Проекция точки (цвета каждого канала) на плоскость вида
-// ax + by + cz = d
-cv::Vec3f project_point(const cv::Vec4f surface, const cv::Vec3f point) {
-    float a = surface[0], b = surface[1], c = surface[2], d = -surface[3];
-    float t = -(a * point[0] + b * point[1] + c * point[2] + d) / (a * a + b * b + c * c);
-    return cv::Vec3f{
-        a * t + point[0],
-        b * t + point[1],
-        c * t + point[2]
-    };
-}
-
-cv::Vec3f rotate_point_y(cv::Vec3f point) {
-    cv::Mat tmp = (cv::Mat_<float>(1, 3) << point[0], point[1], point[2]);
-    tmp = tmp * kRotY;
-    // cv::Vec3f tmp = point.mul(kRotZ);
-    return cv::Vec3f{tmp.at<float>(0, 0), tmp.at<float>(0, 1), tmp.at<float>(0, 2)};
-    // return cv::Vec3f{0, 0, 0};
-    // return tmp;
-}
-
-cv::Vec3f rotate_point_x(cv::Vec3f point) {
-    cv::Mat tmp = (cv::Mat_<float>(1, 3) << point[0], point[1], point[2]);
-    tmp = tmp * kRotX;
-    return cv::Vec3f{tmp.at<float>(0, 0), tmp.at<float>(0, 1), tmp.at<float>(0, 2)};
+cv::Vec3f project_point(cv::Vec3f point) {
+    cv::Mat tmp = (cv::Mat_<float>(3, 1) << point[0], point[1], point[2]);
+    cv::Mat res;
+    cv::gemm(kProj, tmp, 1.0, cv::Mat(), 0.0, res);
+    return cv::Vec3f{res.at<float>(0, 0), res.at<float>(0, 1), res.at<float>(0, 2)};
 }
 
 uchar srgb_to_linrgb(float color) {
@@ -67,37 +36,65 @@ uchar linrgb_to_srgb(float color) {
 }
 
 int main() {
-    std::string fname = "swin.jpg";
+    std::string fname = "waltuh2.jpg";
     cv::Mat3f sample = cv::imread(fname);
-    cv::Mat1b res(1024, 1024, 127);
+    cv::Mat1b res(1024, 1024, 255);
     std::vector<cv::Vec3f> projected_points;
 
-    for (float x = 0; x <= 1; x += 0.01) {
-        for (float y = 0; y <= 1; y += 0.01) {
-            for (float z = 0; z <= 1; z += 0.01) {
-                cv::Vec3f point{x, y, z};
-                point = project_point(cv::Vec4f{2, 2, 2, 3}, point);
-                point = rotate_point_x(point);
-                // point = rotate_point_y(point);
-                // point = project_point(cv::Vec4f{0, 0, 1, 0}, point);
-                cv::Size img_center = cv::Size{int(res.cols - point[0] * res.cols), int(point[1] * res.rows)};
-                cv::circle(res, img_center, 1, 255, 1, cv::FILLED);
+    int x_center = res.cols / 2, y_center = res.rows / 2;
+    // std::cout << x_center << " " << y_center << "\n";
+
+    cv::Mat density_map = cv::Mat::zeros(res.size(), CV_32F);
+
+
+    float max_val = 0.0;
+    
+    float scale = int(res.cols / 3) / (1. + 1. / cv::sqrt(3));
+    for (int y = 0; y < sample.rows; y++) {
+        for (int x = 0; x < sample.cols; x++) {
+            cv::Vec3f point = sample.at<cv::Vec3f>(x, y);
+            cv::Vec3f point_tm = point / 255.;
+            point_tm = project_point(point_tm);
+            int x_c = int(point_tm[0] * scale + x_center);
+            int y_c = int(point_tm[1] * scale + y_center);
+            if (x_c < density_map.cols && x_c >= 0 && y_c < density_map.rows && y_c >= 0){
+                density_map.at<float>(x_c, y_c) += 1.0f;
+            }
+            if (density_map.at<float>(x_c, y_c) >= max_val) {
+                max_val = density_map.at<float>(x_c, y_c);
             }
         }
     }
 
-    for (int y = 0; y < sample.rows; y++) {
-        for (int x = 0; x < sample.cols; x++) {
-            cv::Vec3f point = sample.at<cv::Vec3f>(x, y) / 255.;
-            point = project_point(cv::Vec4f{2, 2, 2, 3}, point);
-            point = rotate_point_x(point);
-            // point = rotate_point_x(point);
-            // point = project_point(cv::Vec4f{0, 0, 1, 0}, point);
-            cv::Size img_center = cv::Size{int(point[0] * res.cols), int(point[1] * res.rows)};
-            cv::circle(res, img_center, 1, 0, 1, cv::FILLED);
-        }
+    // std::cout << max_val << "\n";
+
+    density_map /= max_val;
+    density_map *= 255.f;
+
+    std::vector<cv::Point> points;
+    for (int i = 0; i < 6; i++) {
+        float angle = i * CV_PI / 3.f;
+        int x = int(x_center + res.cols / 3 * std::cos(angle + 0.5 * CV_PI));
+        int y = int(y_center + res.cols / 3 * std::sin(angle + 0.5 * CV_PI));
+        points.push_back(cv::Point(x, y));
     }
-    cv::imshow("swin", res);
+
+    for (int i = 0; i < points.size(); i++) {
+        cv::line(density_map, points[i], points[(i + 1) % points.size()], cv::Scalar(255.f, 255.f, 255.f), 3);
+    }
+
+    cv::Mat1f dens = density_map.clone();
+    cv::imshow("map2", dens);
+    // double mina, maxa;
+    // cv::minMaxLoc(dens, &mina, &maxa);
+
+    // std::cout << dens << "\n" << mina << " " << maxa << "\n";
+    // std::vector<cv::Mat> chan;
+    // cv::split(res, chan);
+    // cv::cvtColor(density_map, density_map, cv::COLOR_RGB2GRAY);
+    // cv::imwrite("map2.jpg", dens);
+    // std::cout << density_map.type() << "\n";
+    // cv::imshow("swin", res);
     cv::waitKey(0);
     return 0;
 }
